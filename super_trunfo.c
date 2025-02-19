bfb2287286c6bb56a17eda106b2aa5d6aff0af6f
Rodrigo Rocha <rrsilva1701@gmail.com>
2025-02-19 20:20:38 -0300

    iniciando desafio supertrunfo

---
+++ super_trunfo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    char estado;
+    char codDaCarta[20];
+    char nomeDaCidade[20];
+    int populacao;
+    float areakm;
+    float pib;
+    int numeroPontosTuristicos;
+    
+
+
+}
